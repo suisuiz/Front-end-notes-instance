549f7921e19d5438d93f34b08af31bdaa6cf9fa1
suisuiz <wangyumin1996@gmail.com>
2022-10-02 22:25:10 +0800

    flexible

---
+++ instance/Public/flexible.js
@@ -0,0 +1,52 @@
+/*
+ * @Descripttion: flexible.js
+ * @Author: SUI
+ * @Date: 2021-10-28 11:05:16
+ * @LastEditors: SUI
+ * @LastEditTime: 2021-10-28 11:41:07
+ * @FilePath: \demo-big-screen\src\common\flexible.js
+ */
+
+(function flexible(window, document) {
+  var docEl = document.documentElement
+  var dpr = window.devicePixelRatio || 1
+
+  // adjust body font size
+  function setBodyFontSize() {
+    if (document.body) {
+      document.body.style.fontSize = (12 * dpr) + 'px'
+    } else {
+      document.addEventListener('DOMContentLoaded', setBodyFontSize)
+    }
+  }
+  setBodyFontSize();
+
+  // set 1rem = viewWidth / 24
+  function setRemUnit() {
+    var rem = docEl.clientWidth / 24
+    docEl.style.fontSize = rem + 'px'
+  }
+
+  setRemUnit()
+
+  // reset rem unit on page resize
+  window.addEventListener('resize', setRemUnit)
+  window.addEventListener('pageshow', function (e) {
+    if (e.persisted) {
+      setRemUnit()
+    }
+  })
+
+  // detect 0.5px supports
+  if (dpr >= 2) {
+    var fakeBody = document.createElement('body')
+    var testElement = document.createElement('div')
+    testElement.style.border = '.5px solid transparent'
+    fakeBody.appendChild(testElement)
+    docEl.appendChild(fakeBody)
+    if (testElement.offsetHeight === 1) {
+      docEl.classList.add('hairlines')
+    }
+    docEl.removeChild(fakeBody)
+  }
+}(window, document))
