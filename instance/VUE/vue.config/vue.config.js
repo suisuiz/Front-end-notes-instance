0e92e55ed3afe80940631ba33a3545c610dee566
suisuiz <wangyumin1996@gmail.com>
2024-03-21 16:12:19 +0800

    feat: update config

--- instance/VUE/vue.config/vue.config.js
+++ instance/VUE/vue.config/vue.config.js
@@ -1,64 +1,122 @@
-// https://cli.vuejs.org/zh/config/#vue-config-js
-const Timestamp = new Date().getTime()
-module.exports = {
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+
+const resolve = (dir) => path.join(__dirname, dir)
+
+module.exports = defineConfig({
+  // 基本路径
+  publicPath: '/',
+  // 输出文件目录
+  outputDir: '',
+  assetsDir: 'static',
+  // eslint-loader 是否在保存的时候检查
   lintOnSave: false,
+  runtimeCompiler: true, // 关键点在这
+  filenameHashing: true,
+  // 调整内部的 webpack 配置。
+  chainWebpack: (config) => {
+    config.resolve.alias.set('@', resolve('src')).set('assets', resolve('src/assets')).set('components', resolve('src/components')).set('types', resolve('src/types')).set('public', resolve('public'))
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => ({
+        ...options,
+        compilerOptions: {
+          // 忽略自定义标签警告 vue3 app.config.compilerOptions.isCustomElement 配置有问题
+          isCustomElement: (tag) => {
+            return ['xml', 'block', 'mutation', 'category'].includes(tag)
+          }
+        }
+      }))
+  },
+  // 生产环境是否生成 sourceMap 文件
   productionSourceMap: false,
-  publicPath: './',
-
-  // 不同的方式 方向代理
-
-  // 1、全部代理
-  // devServer: {
-  //   proxy: 'http://192.168.1.150:18889'
-  // },
-
-  // 2、根据不同的接口前缀 代理
+  // css相关配置
+  css: {
+    // 开启 CSS source maps?
+    sourceMap: false,
+    // css预设器配置项
+    loaderOptions: {
+      scss: {
+        additionalData: `@import './src/assets/styles/variables.scss';`
+      },
+      sass: {
+        implementation: require('sass') // This line must in sass option
+      },
+      css: {
+        modules: {
+          auto: () => false
+        }
+      }
+    }
+  },
+  // webpack-dev-server 相关配置
   devServer: {
+    /* 自动打开浏览器 */
+    open: process.platform === 'darwin',
+    port: 8080,
+    https: false,
+    // hot: 'only',
+    /* 使用代理 */
     proxy: {
-      '/container': {
-        target: 'http://192.168.1.150:7005',
+      '/robogo': {
+        /* 目标代理服务器地址 */
+        target: process.env.VUE_APP_BASE_API,
+        /* 允许跨域 */
         changeOrigin: true,
         ws: true
       },
-      '/api/light-app/transmit': {
-        target: 'http://192.168.1.150:7005',
-        changeOrigin: true,
-        ws: true
-      },
-      '/api/v2/device/service': {
-        target: 'http://192.168.1.150:59881',
-        changeOrigin: true,
-        ws: true
-      },
-      '/api/v2/device/name': {
-        target: 'http://192.168.1.150:59882',
-        changeOrigin: true,
-        ws: true
-      },
-      '/stream': {
-        target: 'http://192.168.1.150:18889',
-        changeOrigin: true,
-        ws: true
-      },
-      '/start': {
-        target: 'http://192.168.1.150:8000',
-        changeOrigin: true,
-        ws: true
-      },
-      '/stop': {
-        target: 'http://192.168.1.150:8000',
+      '/loki': {
+        /* 目标代理服务器地址 */
+        target: process.env.VUE_APP_BASE_API,
+        /* 允许跨域 */
         changeOrigin: true,
         ws: true
       }
     }
   },
-
-  // webpack 配置
   configureWebpack: {
-    output: {
-      // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
-      filename: `static/js/[name].${Timestamp}.js`,
-      chunkFilename: `static/js/[name].${Timestamp}.js`
-    }
-  }
-}
+    resolve: {
+      fallback: {
+        path: require.resolve('path-browserify'),
+        stream: require.resolve('readable-stream'),
+        crypto: require.resolve('crypto-browserify'),
+        perf_hooks: false,
+        module: false,
+        '@blueprintjs/core': false,
+        '@blueprintjs/icons': false,
+        domain: false,
+        fs: false,
+        pnpapi: false,
+        punycode: false
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: 'javascript/auto'
+        }
+      ]
+    },
+    plugins: [
+      new CompressionWebpackPlugin({
+        filename: '[path][base].gzip',
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240, //内容超过10KB进行压缩
+        minRatio: 0.8
+      })
+    ],
+    externals: [
+      {
+        './cptable': 'var cptable'
+      }
+    ]
+  },
+  // 第三方插件配置
+  pluginOptions: {}
+})
